37a1ac2ddac202f8cbc547112bd4a84bb93c3a23
Richiisontheway <mineiriccardo@gmail.com>
2023-12-05 15:52:54 +0100

    esercizio finito

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,45 @@ const row = document.querySelector('.row');
 for (let number = 1; number <= 100; number++) {
     
     const colonna = document.createElement('div');
+    
     colonna.classList.add('col');
-    const card = document.createElement('div')
-    card.classList.add('text-center','')
-    if (number % 3 == 0 && number % 5 == 0)
+
+    row.append(colonna);
+
+    const card = document.createElement('div');
+
+    card.classList.add('d-flex','justify-content-center','width','align-items-center','bg-info');
+
+    colonna.append(card);
+
+    if (number % 3 == 0 && number % 5 == 0){
+
+        card.append('fizzbuzz')
+        card.classList.add('bg-warning')
         console.log('fizzbuzz')
         
-    else if (number % 3 == 0)
-        console.log('fizz')
-        
-    else if (number % 5 == 0)
-        console.log('buzz')
+    }   
 
-    else (
+    else if (number % 3 == 0){
+
+        card.append('fizz')
+        card.classList.add('bg-primary')
+        console.log('fizz')
+
+    }
+        
+    else if (number % 5 == 0){
+
+        card.append('buzz')
+        card.classList.add('bg-danger')
+        console.log('buzz')
+    }
+
+    else {
+
+        card.append(number)
         console.log(number)
-    )
+
+    }
     
 }
